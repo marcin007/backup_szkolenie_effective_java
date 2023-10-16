a12abce35306fdc89eddfb0233729c22f803245f
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-10-16 10:00:03 +0200

    JDK21

--- 01-intro/src/main/java/com/for_comprehension/function/HelloWorld.java
+++ 01-intro/src/main/java/com/for_comprehension/function/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.for_comprehension.function;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        List.of(1,2,3,4);
+
     }
 }
