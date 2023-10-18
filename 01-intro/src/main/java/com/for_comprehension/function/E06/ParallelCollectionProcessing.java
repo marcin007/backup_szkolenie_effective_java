8cf5a3451841994fc263ec967f71c1497ebe55b7
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-10-18 12:20:54 +0200

    20. Parallel Collection Processing - solution

--- 01-intro/src/main/java/com/for_comprehension/function/E06/ParallelCollectionProcessing.java
+++ 01-intro/src/main/java/com/for_comprehension/function/E06/ParallelCollectionProcessing.java
@@ -2,7 +2,14 @@ package com.for_comprehension.function.E06;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 class ParallelCollectionProcessing {
@@ -10,9 +17,36 @@ class ParallelCollectionProcessing {
     public static void main(String[] args) {
         List<Integer> input = List.of(1, 2, 3, 4);
 
-        // TODO process in parallel
-        List<Integer> result = timed(() -> input.stream().map(i -> process(i)).toList());
+        try (ExecutorService e = new ThreadPoolExecutor(0, 1000,
+          1L, TimeUnit.MINUTES,
+          new SynchronousQueue<>())) {
+            List<Integer> result = timed(() -> {
+                List<Future<Integer>> futures = new ArrayList<>();
 
+                for (Integer i : input) {
+                    Future<Integer> r = e.submit(() -> process(i));
+                    futures.add(r);
+                }
+
+                List<Integer> results = new ArrayList<>();
+
+                for (Future<Integer> future : futures) {
+                    try {
+                        results.add(future.get());
+                    } catch (InterruptedException | ExecutionException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+                return results;
+            });
+            System.out.println(result);
+        }
+    }
+
+    public static void syncExample(String[] args) {
+        List<Integer> input = List.of(1, 2, 3, 4);
+        List<Integer> result = timed(() -> input.stream().map(i -> process(i)).toList());
         System.out.println(result);
     }
 
--- 01-intro/src/main/java/com/for_comprehension/function/l8_parallel/L8_ParallelStreams.java
+++ 01-intro/src/main/java/com/for_comprehension/function/l8_parallel/L8_ParallelStreams.java
@@ -3,11 +3,30 @@ package com.for_comprehension.function.l8_parallel;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 class L8_ParallelStreams {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Runtime.getRuntime().availableProcessors());
 
-    public static void main(String[] args) {
+        new Thread(() -> {
+            IntStream.range(0, 20).boxed()
+              .parallel()
+              .map(i -> {
+                  try {
+                      System.out.println(STR."polluting pool with: \{i}");
+                      Thread.sleep(60000);
+                  } catch (InterruptedException e) {
+                      throw new RuntimeException(e);
+                  }
+
+                  return i;
+              }).toList();
+        }).start();
+
+        Thread.sleep(500);
+
         var result = timed(() -> Stream.of(1, 2, 3, 4)
           .parallel()
           .map(i -> process(i))
